0aeb8c8d36b9561f6993aaf68868557dca3074ae
font10 <dfont93@gmail.com>
2023-11-07 12:51:51 +0100

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,8 +9,7 @@ export const verifyToken = async(req, res, next) => {
     //* Separate the token from Bearer
     const token = req.headers.authorization.split(" ")[1]
     //* Verify the token
-    console.log(process.env.JWT_SECRET)
-    jwt.verify(token, "ab12cd3ef45gh6ij7kl89mn10", (err, data) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
         if(err) return res.status(403).json({msg: "Wrong or expired token!!!!"})
         else {
           //* Object with the user info
